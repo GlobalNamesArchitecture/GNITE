67d4578c9a367377afd0387b22c9670200c0f1fe
Jason Morrison <jmorrison@thoughtbot.com>
2010-07-28 16:25:22 -0400

    move DO_NOT_REPLY from environment.rb to initializer

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Gnite::Application.initialize!
-
-DO_NOT_REPLY = "do-not-reply@gnite.globalnames.org"
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,5 @@
+DO_NOT_REPLY = "do-not-reply@gnite.globalnames.org"
+
 Clearance.configure do |config|
   config.mailer_sender = DO_NOT_REPLY
 end
