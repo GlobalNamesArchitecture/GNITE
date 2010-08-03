b46e2a2e7933aae27845533c7a3934d9b9ec767d
Jason Morrison <jmorrison@thoughtbot.com>
2010-08-03 10:29:12 -0400

    Cleanup tree creation code

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -2,8 +2,6 @@ class NodesController < ApplicationController
   def index
     respond_to do |format|
       format.json do
-        # root = Node.find(params[:parent_id]) rescue nil
-        # nodes = root ? root.children : Node.roots
         nodes = current_user.trees.find(params[:tree_id]).nodes
 
         node_hashes = nodes.map do |node|
@@ -11,9 +9,6 @@ class NodesController < ApplicationController
             :data => node.name,
             :attr => { :id => node.id }
           }
-
-          # node_hash[:state] = 'closed' if node.has_children?
-          # node_hash
         end
 
         render :json => node_hashes
@@ -21,20 +16,14 @@ class NodesController < ApplicationController
     end
   end
 
-  # prototype:
-  #
   def create
     respond_to do |format|
       format.json do
         node_attributes = params[:node].merge(:tree_id => params[:tree_id])
         node = Node.new(node_attributes)
 
-        # if
         node.save
         render :json => node, :status => :created
-        #else
-        #  render :json => node.errors, :status => :unprocessable_entity
-        #end
       end
     end
   end
