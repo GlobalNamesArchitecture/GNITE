b092e3dcb10b1e447c5dc418af658f090feaa7f7
Harold Giménez <hgimenez@thoughtbot.com>
2010-09-15 09:18:19 -0400

    Update gnaclr classification fixtures to include revisions

--- spec/models/gnaclr_classification_spec.rb
+++ spec/models/gnaclr_classification_spec.rb
@@ -4,20 +4,20 @@ describe GnaclrClassification, "with remote classifications" do
   let(:expected_classifications) do
     [
       GnaclrClassification.new({
-        "title" => "NCBI",
-        "authors" => [{ "first_name" => "Dmitry", "last_name" => "Mozzherin", "email" => "dmozzherin@gmail.com"}],
-        "description" => "NCBI classification",
-        "uuid" => "97d7633b-5f79-4307-a397-3c29402d9311",
-        "updated" => Time.parse("Thu Jul 15 20:49:40 UTC 2010"),
-        "file_url" => "http://gnaclr.globalnames.org/files/baaa4762cb1ef8118f2c20257a3c27c946f09375/ncbi.tar.gz"
+        "title"       => "Spider Classification",
+        "authors"     => [{ "first_name" => "David", "last_name" => "Shorthouse", "email" => "dshorthouse@eol.org"}],
+        "description" => "paraThis is a sample classification used as a template for inclusion in the Global Names Architecture Classification and List Repository.",
+        "uuid"        => "853437dc-6d9f-4ab5-ba30-5ae006fccae2",
+        "updated"     => Time.parse("Tue Sep 14 04:11:40 UTC 2010"),
+        "file_url"    => "http://gnaclr.globalnames.org/files/853437dc-6d9f-4ab5-ba30-5ae006fccae2/853437dc-6d9f-4ab5-ba30-5ae006fccae2.gzip"
       }),
       GnaclrClassification.new({
-        "title" => "Choreutidae Classification",
-        "authors" => [{"first_name" => "Jadranka", "last_name" => "Rota", "email" => "jadranka.rota@gmail.com"}],
-        "description" => "&lt;p&gt;based on Heppner and Duckworth and new genera published since&lt;/p&gt;",
-        "uuid" => "91d08561-b72f-4159-9461-119690b67afa",
-        "updated" => Time.parse("Wed May 26 14:57:31 UTC 2010"),
-        "file_url" => "http://gnaclr.globalnames.org/files/d6781d6071d25a0e46dcb889d6bd1a40407f1b6e/choreutidae.tar.gz"
+        "title"       => "Index Fungorum",
+        "authors"     => [{"first_name" => "Paul", "last_name" => "Kirk", "email" => "p.kirk@cabi.org"}],
+        "description" => "Classification of Fungi",
+        "uuid"        => "a9995ace-f04f-49e2-8e14-4fdbc810b08a",
+        "updated"     => Time.parse("Wed Sep 08 15:08:05 UTC 2010"),
+        "file_url"    => "http://gnaclr.globalnames.org/files/a9995ace-f04f-49e2-8e14-4fdbc810b08a/index_fungorum.tar.gz"
       })
     ]
   end
@@ -28,21 +28,20 @@ describe GnaclrClassification, "with remote classifications" do
     stub_app.register_resource("/classifications?format=xml", response, "application/xml")
   end
 
-  it "should return all the classifications" do
+  it "returns all the classifications" do
     GnaclrClassification.all.size.should == 2
     GnaclrClassification.all.should == expected_classifications
   end
 
-  it "should find a classification by uuid" do
-    GnaclrClassification.find_by_uuid('97d7633b-5f79-4307-a397-3c29402d9311').should == expected_classifications.first
-    GnaclrClassification.find_by_uuid('91d08561-b72f-4159-9461-119690b67afa').should == expected_classifications.last
+  it "finds a classification by uuid" do
+    GnaclrClassification.find_by_uuid('853437dc-6d9f-4ab5-ba30-5ae006fccae2').should == expected_classifications.first
+    GnaclrClassification.find_by_uuid('a9995ace-f04f-49e2-8e14-4fdbc810b08a').should == expected_classifications.last
   end
 end
 
 describe GnaclrClassification, "attributes" do
-  before do
-    @updated_time = Time.now
-    @attributes = {
+  let(:attributes) do
+    {
       :title       => "Title",
       :authors     => ["Author 1", "Author 2"],
       :description => "Description",
@@ -51,12 +50,14 @@ describe GnaclrClassification, "attributes" do
       :file_url    => 'example.tar.gz'
     }
   end
+  before do
+    @updated_time  = Time.now
+  end
 
-  it "should have a title, authors, a description, an updated, a uuid, a file URL, and a constructor that accepts a hash" do
+  it "has a title, authors, a description, an updated, a uuid, a file URL and a constructor that accepts a hash" do
+    gnaclr_classification = GnaclrClassification.new(attributes)
 
-    gnaclr_classification = GnaclrClassification.new(@attributes)
-
-    gnaclr_classification.attributes.should  == @attributes
+    gnaclr_classification.attributes.should  == attributes
 
     gnaclr_classification.title.should       == "Title"
     gnaclr_classification.authors.should     == ["Author 1", "Author 2"]
@@ -66,18 +67,18 @@ describe GnaclrClassification, "attributes" do
     gnaclr_classification.file_url.should    == "example.tar.gz"
   end
 
-  it "should test equality on attributes" do
-    one = GnaclrClassification.new(@attributes)
-    another = GnaclrClassification.new(@attributes)
+  it "tests equality on attributes" do
+    one     = GnaclrClassification.new(attributes)
+    another = GnaclrClassification.new(attributes)
 
     one.should == another
   end
 
-  it "should provide uuid for #to_param" do
+  it "provides uuid for #to_param" do
     GnaclrClassification.new(:uuid => "uuid").to_param.should == "uuid"
   end
 
-  it "should default its revisions to []" do
+  it "defaults its revisions to []" do
     GnaclrClassification.new.revisions.should == []
   end
 end
