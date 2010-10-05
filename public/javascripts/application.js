6193d6ab7e5f360933ce8474ea30d4344bbffa10
Tristan Dunn <tdunn@thoughtbot.com>
2010-10-05 15:38:10 -0400

    A lot more JS clean up.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -153,7 +153,6 @@ $(function() {
   /*
    * Reference Trees
    */
-  // TODO: Clean up.
   $('.reference-tree .jstree-clicked').live('click', function() {
     var self     = $(this);
     var tree     = self.parents('.reference-tree');
@@ -188,17 +187,18 @@ $(function() {
         container.spinner();
 
         $.ajax({
-          url: '/search',
-          type: 'GET',
-          data: { 'search_term' : term, 'master_tree_id' : master_tree_id },
-          success: function(results) {
-            container.unspinner();
-            $("#gnaclr-error").hide();
+          url     : '/search',
+          type    : 'GET',
+          data    : { 'search_term' : term, 'master_tree_id' : master_tree_id },
+          success : function(results) {
+            $('#gnaclr-error').hide();
             $('#new-tab .tree-background').html(results);
           },
-          error: function(){
+          error : function() {
+            $('#gnaclr-error').show();
+          },
+          complete : function() {
             container.unspinner();
-            $("#gnaclr-error").show();
           }
         });
       }
@@ -217,11 +217,11 @@ $(function() {
    */
   var master_tree_id = $('#tree-container').attr('data-database-id');
 
-  $("#add-node").click(function() {
-    $("#master-tree").jstree("create");
+  $('#add-node').click(function() {
+    $('#master-tree').jstree('create');
   });
 
-  $("#master-tree").jstree($.extend(true, {}, GNITE.MasterTree, {
+  $('#master-tree').jstree($.extend(true, {}, GNITE.MasterTree, {
     'json_data': {
       'ajax' : {
         'url' : '/master_trees/' + master_tree_id + '/nodes.json'
@@ -229,88 +229,91 @@ $(function() {
     }
   }));
 
-  $("#master-tree")
-  .bind("create.jstree", function(event, data) {
-    var newNode = data.rslt;
+  $('#master-tree')
+  .bind('create.jstree', function(event, data) {
+    var node     = data.rslt;
+    var name     = node.name;
+    var parentID = null;
 
-    var parentId;
-    if (newNode.parent == -1) {
-      parentId = null;
-    } else {
-      parentId = newNode.parent.attr("id");
+    if (node.parent != -1) {
+      parentID = node.parent.attr('id');
     }
 
     $.ajax({
-      type: "POST",
-      url: '/master_trees/' + master_tree_id + '/nodes.json',
-      contentType: "application/json",
-      dataType: 'json',
-      data: JSON.stringify({node: {name: { name_string : newNode.name }, parent_id: parentId}}),
-      success: function(data, textStatus, xhr) {
-        newNode.obj.attr("id", data.node.id);
+      type        : 'POST',
+      url         : '/master_trees/' + master_tree_id + '/nodes.json',
+      data        : JSON.stringify({ 'node' : { 'name' : { 'name_string' : name }, 'parent_id' : parentID } }),
+      contentType : 'application/json',
+      dataType    : 'json',
+      success     : function(data) {
+        node.obj.attr('id', data.node.id);
       }
     });
   });
 
-  $("#master-tree").bind("loaded.jstree", function(event, data) {
-    $("#master-tree").addClass("loaded");
+  $('#master-tree').bind('loaded.jstree', function(event, data) {
+    $('#master-tree').addClass('loaded');
   });
 
-  $("#master-tree").bind("rename.jstree", function(event, data) {
-    var updatedNode = data.rslt;
+  $('#master-tree').bind('rename.jstree', function(event, data) {
+    var node     = data.rslt;
+    var id       = node.obj.attr('id');
+    var new_name = node.new_name;
+
     $.ajax({
-      type: "POST",
-      url: "/master_trees/" + master_tree_id + "/nodes/"+updatedNode.obj.attr("id")+"/name_updates.json",
-      contentType: "application/json",
-      dataType: 'json',
-      data: JSON.stringify({name: { name_string: updatedNode.new_name } }),
-      success: function(data, textStatus, xhr) {
-        updatedNode.obj.attr("id", data.node.id);
+      type        : 'POST',
+      url         : '/master_trees/' + master_tree_id + '/nodes/' + id + '/name_updates.json',
+      data        : JSON.stringify({ 'name' : { 'name_string' : new_name } }),
+      contentType : 'application/json',
+      dataType    : 'json',
+      success     : function(data) {
+        updatedNode.obj.attr('id', data.node.id);
       }
     });
   });
 
-  $("#master-tree").bind("move_node.jstree", function(event, data) {
-     var result = data.rslt;
-     var movedNodeId = result.o.attr("id");
-     var parentId = result.np.attr("id");
-     var isCopy = data.rslt.cy;
-     if(parentId == "master-tree"){
-       parentId = null;
+  $('#master-tree').bind('move_node.jstree', function(event, data) {
+     var result      = data.rslt;
+     var isCopy      = result.cy;
+     var parentID    = result.np.attr('id');
+     var movedNodeID = result.o.attr('id');
+
+     if (parentID == 'master-tree') {
+       parentID = null;
      }
+
+     var url = '/master_trees/' + master_tree_id + '/nodes/' + movedNodeID;
 
      if (isCopy) {
-       $.ajax({
-         type: "POST",
-         url: "/master_trees/" + master_tree_id + "/nodes/" + movedNodeId + "/clone.json",
-         contentType: "application/json",
-         dataType: 'json',
-         data: JSON.stringify({node: {parent_id: parentId }}),
-         success: function(data, textStatus, xhr) {
-           result.oc.attr('id', data.node.id);
-         }
-       });
-     } else {
-       $.ajax({
-         type: "PUT",
-         url: "/master_trees/" + master_tree_id + "/nodes/"+result.o.attr("id")+".json",
-         contentType: "application/json",
-         dataType: 'json',
-         data: JSON.stringify({node: {parent_id: parentId }}),
-         success: function(data, textStatus, xhr) {
-           result.o.attr("id", data.node.id);
-         }
-       });
+       url += '/clone';
      }
+
+     url += '.json';
+
+     $.ajax({
+       type        : isCopy ? 'POST' : 'PUT',
+       url         : url,
+       data        : JSON.stringify({ 'node' : { 'parent_id' : parentID } }),
+       contentType : 'application/json',
+       dataType    : 'json',
+       success     : function(data) {
+         if (isCopy) {
+           result.oc.attr('id', data.node.id);
+         } else {
+           result.o.attr('id', data.node.id);
+         }
+       }
+     });
   });
 
-  $("#master-tree").bind("remove.jstree", function(event, data) {
+  $('#master-tree').bind('remove.jstree', function(event, data) {
     var node = data.rslt;
+    var id   = node.obj.attr('id');
+
     $.ajax({
-      type: "DELETE",
-      url: "/master_trees/" + master_tree_id + "/nodes/"+node.obj.attr("id")+".json",
-      contentType: "application/json",
-      dataType: 'json',
+      type        : 'DELETE',
+      url         : '/master_trees/' + master_tree_id + '/nodes/' + id + '.json'//,
+      //contentType : 'application/json'
     });
   });
 
@@ -324,22 +327,20 @@ $(function() {
   });
 
   $('#master-tree .jstree-clicked').live('dblclick', function() {
-    $(".jstree-focused").jstree("rename");
+    $('.jstree-focused').jstree('rename');
   });
 
   $('#add-node-wrap').live('click', function(event) {
-    var hitNode = (event.target.tagName == 'A') ||
-                  (event.target.tagName == 'INS');
+    var target = $(event.target);
 
-    if (!hitNode) {
-      $(event.target).closest('.jstree').jstree('deselect_all');
-      $('#add-node-wrap + .node-metadata').hide();
+    if (event.target.tagName != 'A' && event.target.tagName != 'INS') {
       $('#add-node-wrap').css('bottom', '20px');
-    }
-  });
+      $('#add-node-wrap + .node-metadata').hide();
 
-  $('#add-node-wrap').live('click', function(event) {
-    $(event.target).find('.jstree').jstree('deselect_all');
+      target.closest('.jstree').jstree('deselect_all');
+    }
+
+    target.find('.jstree').jstree('deselect_all');
   });
 
   $('#master_tree_title')
@@ -380,26 +381,15 @@ $(function() {
   /*
    * Miscellaneous
    */
-  $('#browse-gnaclr-button').live('click', function() {
-    var url = $(this).attr("href");
-    $('#new-tab').load(url);
+  $('#browse-gnaclr-button, .ajax-load-new-tab, .gnaclr_classification_show').live('click', function() {
+    $('#new-tab').load($(this).attr('href'));
+
     return false;
   });
 
-  $('.ajax-load-new-tab').live('click', function() {
-    var url = $(this).attr("href");
-    $('#new-tab').load(url);
-    return false;
-  });
-
-  $('.gnaclr_classification_show').live('click', function() {
-    var url = $(this).attr("href");
-    $('#new-tab').load(url);
-    return false;
-  });
-
-  var importTree = function(opts){
+  var importTree = function(opts) {
     opts.spinnedElement.spinner()
+
     $.post('/gnaclr_imports', { master_tree_id : opts.master_tree_id, title : opts.title, url : opts.url }, function(response) {
       var tree_id = response.tree_id;
       var timeout = setTimeout(function checkImportStatus() {
@@ -416,78 +406,15 @@ $(function() {
             $('#tab-titles li:first-child').show();
             $('#tabs li:first-child ul').append('<li><a href="#' + response.domid + '">' + response.title + '</a></li>');
 
-            $("#container_for_" + response.domid).jstree({
-              "json_data": {
-                "ajax": {
-                  "url": response.url,
-                  "data" : function (node) {
-                    if (node.attr) {
-                      return { parent_id : node.attr("id") };
-                    } else {
-                      return {};
-                    }
+            $('#container_for_' + response.domid).jstree($.extend(true, {},
+              GNITE.ReferenceTree, {
+                'json_data' : {
+                  'ajax' : {
+                    'url' : response.url
                   }
-                },
-              },
-              "crrm": {
-                "move": {
-                  "check_move": function(move) { return false; },
-                  "always_copy": "multitree"
-                },
-              },
-              "plugins" : [ "themes", "json_data", "ui", "dnd", "crrm"]
-            });
-
-            $('#container_for_' + response.domid + ' .jstree-clicked').live('click', function() {
-              var self     = $(this);
-              var metadata = $('#' + response.domid + ' .node-metadata');
-
-              metadata.spinner();
-
-              $.getJSON('/reference_trees/' + tree_id + '/nodes/' + self.parent('li').attr('id'), function(data) {
-                var rank             = $.trim(data.rank);
-                var synonyms         = data.synonyms;
-                var vernacular_names = data.vernacular_names;
-
-                metadata.find('.metadata-section ul').empty();
-
-                if (synonyms.length == 0) {
-                  metadata.find('.metadata-synonyms ul').append('<li>None</li>');
-                } else {
-                  $.each(synonyms, function(index, synonym) {
-                    metadata.find('.metadata-synonyms ul').append('<li>' + synonym + '</li>');
-                  });
                 }
-
-                if (vernacular_names.length == 0) {
-                  metadata.find('.metadata-vernacular-names ul').append('<li>None</li>');
-                } else {
-                  $.each(vernacular_names, function(index, vernacular_name) {
-                    metadata.find('.metadata-vernacular-names ul').append('<li>' + vernacular_name + '</li>');
-                  });
-                }
-
-                if (rank == '') {
-                  metadata.find('.metadata-rank ul').append('<li>None</li>');
-                } else {
-                  metadata.find('.metadata-rank ul').append('<li>' + rank + '</li>');
-                }
-
-                var wrapper = $('#container_for_' + response.domid);
-
-                metadata.show();
-                wrapper.css('bottom', metadata.height());
-
-                var nodePosition  = self[0].offsetTop - wrapper[0].scrollTop;
-                var visibleHeight = wrapper.height();
-
-                if (nodePosition >= visibleHeight) {
-                  wrapper[0].scrollTop += nodePosition - visibleHeight + 36;
-                }
-
-                metadata.unspinner();
-              });
-            });
+              }
+            ));
 
             $('#tabs li:first-child ul li:last-child a').trigger('click');
           } else if (xhr.status == 204) {
@@ -503,18 +430,19 @@ $(function() {
     $('#tree-newimport').spinner();
     $('#import-gnaclr-button').remove();
 
-    var checkedRadioButton = $("#tree-revisions form input:checked");
-    var opts = { master_tree_id : $("#tree-container").attr("data-database-id"),
+    var checkedRadioButton = $('#tree-revisions form input:checked');
+    var opts = { master_tree_id : $('#tree-container').attr('data-database-id'),
                  title          : $('#gnaclr-description h2').text(),
                  url            : checkedRadioButton.attr('data-tree-url'),
-                 spinnedElement : $("#tree-newimport") };
+                 spinnedElement : $('#tree-newimport') };
+
     importTree(opts);
   });
 
   $('#search-nav li').live('click', function() {
-    var target = $(this).find('a').attr('href');
     $('.search-text').fadeOut('fast');
-    $(target).fadeIn('fast');
+    $($(this).find('a').attr('href')).fadeIn('fast');
+
     return false;
   });
 
@@ -524,7 +452,9 @@ $(function() {
                  title          : self.parent().siblings('table').find('.current-name').text(),
                  url            : self.attr('data-tree-url'),
                  spinnedElement : $('#search-results') };
+
     importTree(opts);
+
     return false;
   });
 });
