2eecfe55e2fb408371b50a5851878a624205fa14
Tristan Dunn <tdunn@thoughtbot.com>
2010-10-05 12:29:09 -0400

    Handle missing names for synonyms and vernacular names in Nodes#show.

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -29,8 +29,8 @@ class NodesController < ApplicationController
 
     render :json => {
       :rank             => node.rank,
-      :synonyms         => node.synonyms.map(&:name).map(&:name_string),
-      :vernacular_names => node.vernacular_names.map(&:name).map(&:name_string)
+      :synonyms         => node.synonyms.map(&:name).compact.map(&:name_string),
+      :vernacular_names => node.vernacular_names.map(&:name).compact.map(&:name_string)
     }
   end
 
