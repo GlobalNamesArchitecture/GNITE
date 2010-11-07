04d0dcfcf5347ed9249c6cd8d6d7c135edb8dd1a
Dmitry Mozzherin <dmozzherin@gmail.com>
2010-11-06 20:18:37 -0400

    updates in gems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     qualified_controller_name = controller.controller_path.gsub('/','-')
     "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name}"
   end
+
+  def meta_description
+    "GNITE is a web application that allows biologists and taxonomists to view, edit, and add data related to the classification of all known biological species."
+  end
 end
