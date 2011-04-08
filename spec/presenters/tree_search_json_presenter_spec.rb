5af52213e580bda3a4a3afdbb03b95d61d0f3a2f
David Shorthouse <dshorthouse@eol.org>
2011-04-08 14:41:11 -0400

    More appropriate tree_search_json_presenter spec

--- spec/presenters/tree_search_json_presenter_spec.rb
+++ spec/presenters/tree_search_json_presenter_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe TreeSearchJsonPresenter do
   it "should present tree search results as JSON" do
-    node = Factory(:node, :name => Factory(:name, :name_string => 'node-name'))
-    nodes = [node]
+    parent = Factory(:node, :name => Factory(:name, :name_string => 'parent'))
+    child = Factory(:node, :parent => parent, :name => Factory(:name, :name_string => 'child'))
+    nodes = [child] #simulate search for child name
 
-    expected_hash = [{"name"=>"node-name", "id"=> node.id, "treepath"=>{"name_strings"=>"node-name", "node_ids"=>["#" + node.id.to_s]}}]
+    expected_hash = [{"name" => "child", "id" => child.id, "treepath" => { "name_strings" => "parent > child", "node_ids"=>["#" + parent.id.to_s, "#" + child.id.to_s] } } ]
     presented_hash = JSON.parse(TreeSearchJsonPresenter.present(nodes))
 
     presented_hash.should == expected_hash
