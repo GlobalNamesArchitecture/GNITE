88669914e139ea3dbeee42351fa19bed96d80bc8
David Shorthouse <davidpshorthouse@gmail.com>
2011-08-23 17:24:22 -0400

    WIP removed defunct files

--- app/controllers/confirmations_controller.rb
+++
@@ -1,7 +0,0 @@
-class ConfirmationsController < Clearance::ConfirmationsController
-  private
-
-  def url_after_create
-    master_trees_url
-  end
-end
--- app/controllers/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class SessionsController < Clearance::SessionsController
-  layout 'login'
-
-  private
-
-  def url_after_create
-    master_trees_url
-  end
-end
