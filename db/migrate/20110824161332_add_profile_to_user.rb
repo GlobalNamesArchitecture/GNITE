e58e86b3f2959134b6fa2e53d238c2b5620dc927
David Shorthouse <davidpshorthouse@gmail.com>
2011-08-26 11:07:29 -0400

    Removed admin column from migration

--- db/migrate/20110824161332_add_profile_to_user.rb
+++ db/migrate/20110824161332_add_profile_to_user.rb
@@ -1,13 +1,11 @@
 class AddProfileToUser < ActiveRecord::Migration
   def self.up
-    add_column :users, :admin, :boolean, :default => false
     add_column :users, :given_name, :string, :limit => 128
     add_column :users, :surname, :string, :limit => 128
     add_column :users, :affiliation, :string
   end
 
   def self.down
-    remove_column :users, :admin
     remove_column :users, :given_name
     remove_column :users, :surname
     remove_column :users, :affiliation
