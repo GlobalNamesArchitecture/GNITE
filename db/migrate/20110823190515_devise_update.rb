2a2b8d3b6c0b882e01f7b8ec7bae2373f9dbd782
David Shorthouse <davidpshorthouse@gmail.com>
2011-08-26 11:12:59 -0400

    Fixed migration

--- db/migrate/20110823190515_devise_update.rb
+++ db/migrate/20110823190515_devise_update.rb
@@ -43,6 +43,8 @@ class DeviseUpdate < ActiveRecord::Migration
     remove_column :users, :reset_password_sent_at
     remove_column :users, :remember_created_at
     remove_column :users, :sign_in_count
+    remove_column :users, :current_sign_in_ip
+    remove_column :users, :last_sign_in_ip
     remove_column :users, :current_sign_in_at
     remove_column :users, :last_sign_in_at
     remove_column :users, :confirmed_at
