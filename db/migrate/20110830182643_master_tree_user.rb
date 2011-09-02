0cdb3341651298f07f4b0428ff92275739e90c60
David Shorthouse <davidpshorthouse@gmail.com>
2011-09-02 13:23:09 -0400

    Removed comment from migration

--- db/migrate/20110830182643_master_tree_user.rb
+++ db/migrate/20110830182643_master_tree_user.rb
@@ -3,8 +3,7 @@ class MasterTreeUser < ActiveRecord::Migration
     change_table :trees do |t|
       t.references :user
     end
-    
-    #update trees to have a user_id for the owner - how?
+
     execute "UPDATE trees AS t INNER JOIN (SELECT * FROM master_tree_contributors mtc2 GROUP BY mtc2.master_tree_id) AS mtc ON t.id = mtc.master_tree_id SET t.user_id = mtc.user_id WHERE t.type = 'MasterTree'"
   end
 
